3fb1157ea36fefc628efdf5e46b7d1b0e6e36921
Omar Sainz <SQL_Slammer.exe@live.de>
2015-11-02 15:58:32 -0600

    Advanced Segues project added

---
+++ Advanced Segues/Advanced Segues/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Advanced Segues
+//
+
+import UIKit
+
+var rowCounter:Int = 0
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print(rowCounter)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
